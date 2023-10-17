f18672b4e9403b31ca3f8161642982eeedccebca
HonorThe1nsane <troymd2009@hotmail.com>
2023-10-17 12:02:41 -0600

    initial commit

---
+++ main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let x = 5;
+    let y = 7;
+    let result = x + y;
+    println! ("The result is: {}", result);
+    if result > 10 {
+    println! ("The result is greater than 10");
+    } else {
+    println! ("The result is less than or equal to 10");
+    }
+    let mut i = 0;
+    while i < 5 { println! ("i is {}", i);
+    i += 1;
+}
+ for j   in 0..5 {
+    println! ("j is {}'", j);
+ }}
